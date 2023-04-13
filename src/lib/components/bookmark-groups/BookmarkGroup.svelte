a7a5948c8a90ec4aa981e52d67e952aee790ff92
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-13 16:24:53 +0200

    created bookmark cards

--- src/lib/components/bookmark-groups/BookmarkGroup.svelte
+++ src/lib/components/bookmark-groups/BookmarkGroup.svelte
@@ -1,9 +1,16 @@
-<div class="bg-primary-subtle grid rounded p-4">
-    <div class="d-flex justify-content-between">
-        <h2>My Favorite Bookmarks</h2>
-        <button class="btn btn-success">
-            New Bookmark
-        </button>
-    </div>
+<script>
+  import Bookmark from "../bookmarks/Bookmark.svelte";
+</script>
 
+<div class="bg-primary-subtle rounded p-4">
+  <div class="d-flex justify-content-between">
+    <h2>My Favorite Bookmarks</h2>
+    <button class="btn btn-success">New Bookmark</button>
+  </div>
+  <div class="row row-cols-2 row-gap-4 my-4">
+    <Bookmark />
+    <Bookmark />
+    <Bookmark />
+    <Bookmark />
+  </div>
 </div>
---
+++ src/lib/components/bookmarks/Bookmark.svelte
@@ -0,0 +1,15 @@
+<div class="col">
+  <div class="card" style="width: 24rem;">
+    <div class="card-body d-flex justify-content-between">
+      <h5 class="card-title text-truncate my-auto">Bookmark</h5>
+      <div class="d-flex gap-2">
+        <button class="btn btn-secondary">
+          <i class="bi bi-pencil-fill" />
+        </button>
+        <button class="btn btn-danger">
+          <i class="bi bi-trash-fill" />
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
