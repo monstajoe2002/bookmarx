edbf6c718f808dcdf15a8734bc10b28c43c18580
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-13 02:38:28 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+ 
+</script>
+
+<div>
+  <h1>Hello</h1>
+</div>
+
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), crx({ manifest })],
+});
