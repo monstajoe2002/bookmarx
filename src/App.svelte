3ea214611f5de6b5f3bbbaac89acf073681fbc54
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-13 16:52:41 +0200

    create a fallback component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Container from "./layout/Container.svelte";
   import BookmarkGroup from "./lib/components/bookmark-groups/BookmarkGroup.svelte";
+  import BookmarkGroupFallback from "./lib/components/bookmark-groups/BookmarkGroupFallback.svelte";
   import ActiveTabs from "./lib/components/tabs/ActiveTabs.svelte";
 </script>
 
 <Container>
   <ActiveTabs/>
-  <BookmarkGroup/>
+  <!-- <BookmarkGroup/> -->
+  <BookmarkGroupFallback/>
 </Container>
 
---
+++ src/lib/components/bookmark-groups/BookmarkGroupFallback.svelte
@@ -0,0 +1,6 @@
+<div class="d-flex justify-content-center align-items-center my-auto">
+  <div class="text-center">
+    <i class="bi bi-bookmarks-fill text-muted" style="font-size: 10rem;" />
+    <h1 class="text-muted">No bookmarks groups yet</h1>
+  </div>
+</div>
