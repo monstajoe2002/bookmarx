4473110d90069cf0c66c14ca83a2e196a091444a
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-14 14:41:36 +0200

    created modals for new bookmark and bookmark group

---
+++ src/lib/components/misc/ModalButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Button, Modal, Label, Input, Checkbox } from 'flowbite-svelte'
+    let formModal = false;
+    export let text:string;
+  </script>
+  
+  <Button on:click={() => formModal = true}>{text}</Button>
+  <Modal bind:open={formModal} size="xs" autoclose={false} class="w-full">
+    <slot/>
+  </Modal>
