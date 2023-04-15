90a0bc5641f78a9b002d532d31fd11900eec7035
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-15 14:52:16 +0200

    using gray text instead of decreasing opacity

--- src/lib/components/bookmark-groups/BookmarkGroupFallback.svelte
+++ src/lib/components/bookmark-groups/BookmarkGroupFallback.svelte
@@ -1,6 +1,6 @@
 <div class="text-center max-h-screen my-auto ">
-  <i class="bi bi-bookmarks-fill text-9xl opacity-25" />
-  <h1 class="text-4xl font-bold opacity-25 mt-8">
+  <i class="bi bi-bookmarks-fill text-9xl text-slate-300 " />
+  <h1 class="text-4xl font-bold text-slate-300 mt-8">
     No bookmark groups yet. Click 'New Bookmark Group' above to create one.
   </h1>
 </div>
