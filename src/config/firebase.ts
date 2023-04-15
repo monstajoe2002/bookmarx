899c2f31e995f1a5806d33e8188fbe067ce83414
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-15 15:00:39 +0200

    Firebase config setup

---
+++ src/config/firebase.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+  deleteApp(app);
+  app = initializeApp(firebaseConfig);
+}
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
