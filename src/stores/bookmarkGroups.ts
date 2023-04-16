4b6bbcaf5cbf6f0499ce603d7f9e24e881cd9593
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-16 22:12:36 +0200

    create document using uuid

--- src/stores/bookmarkGroups.ts
+++ src/stores/bookmarkGroups.ts
@@ -1,14 +1,16 @@
 import { writable } from "svelte/store";
 import { db } from "../config/firebase";
-import { addDoc, collection, doc, getDocs } from "firebase/firestore";
-
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
+import { v4 as uuidV4 } from "uuid";
 const querySnapshot = await getDocs(collection(db, "bookmarkGroups"));
 const data = querySnapshot.docs.map((doc) => doc.data() as BookmarkGroup);
 
 export const bookmarkGroups = writable<BookmarkGroup[]>(data);
 
 export const createBookmarkGroup = async (name: string) => {
-  await addDoc(collection(db, "bookmarkGroups"), {
+  const bookmarkGroupRef = doc(db, "bookmarkGroups", uuidV4());
+  await setDoc(bookmarkGroupRef, {
+    id: bookmarkGroupRef.id,
     name,
     bookmarks: doc(collection(db, "bookmarks")),
   });
