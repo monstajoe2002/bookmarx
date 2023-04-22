05d1429657c4d7ed2cadb5ffd3d3cdab9e18107c
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-22 14:51:18 +0200

    added edit icon next to group name

--- src/lib/components/bookmark-groups/BookmarkGroup.svelte
+++ src/lib/components/bookmark-groups/BookmarkGroup.svelte
@@ -17,7 +17,51 @@
 
 <div class="bg-blue-200 rounded-md p-6 max-w-screen my-6">
   <div class="flex justify-between">
-    <h1 class="text-4xl font-semibold dark:text-white">{name}</h1>
+    <div class="flex gap-4">
+      <h1 class="text-4xl font-semibold dark:text-white">{name}</h1>
+      <ModalButtonWithIcon>
+        <svelte:fragment slot="icon">
+          <i class="bi bi-pencil-fill" />
+        </svelte:fragment>
+        <svelte:fragment slot="content">
+          <form
+            class="flex flex-col space-y-6"
+            action="#"
+            on:submit|preventDefault={() => createBookmarkGroup(name)}
+          >
+            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+              Edit Group Name
+            </h3>
+            <Label class="space-y-2">
+              <span>Name</span>
+              <Input
+                type="text"
+                name="name"
+                placeholder="Example"
+                required
+                bind:value={name}
+              />
+            </Label>
+            <Button type="submit" class="w-full1">Create</Button>
+          </form>
+        </svelte:fragment>
+        {#if showSuccess}
+          <Alert border color="green">
+            <span slot="icon">
+              <i class="bi bi-check-circle-fill" />
+            </span>
+            <span class="font-medium">Bookmark saved!</span>
+          </Alert>
+        {/if}
+        {#if showError}
+          <ErrorAlert>
+            <svelte:fragment slot="message">
+              An error occured while creating the bookmark.
+            </svelte:fragment>
+          </ErrorAlert>
+        {/if}
+      </ModalButtonWithIcon>
+    </div>
     <div class="flex gap-4">
       <!-- Custom component for the new bookmark modal -->
       <ModalButton text="New Bookmark">
