7b718ed98ba06b6e3ea9e7d4d9a03ee7e3dd4cb0
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-22 14:43:03 +0200

    added an auth fallback

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
+  import { Alert } from "flowbite-svelte";
   import Container from "./layout/Container.svelte";
   import BookmarkGroup from "./lib/components/bookmark-groups/BookmarkGroup.svelte";
   import BookmarkGroupFallback from "./lib/components/bookmark-groups/BookmarkGroupFallback.svelte";
   import ActiveTabs from "./lib/components/tabs/ActiveTabs.svelte";
+  import { authStore } from "./stores/authStore";
   import { bookmarkGroups } from "./stores/bookmarkGroups";
+  import AuthButtonGroup from "./lib/components/auth/AuthButtonGroup.svelte";
 </script>
 
 <Container>
   <ActiveTabs />
-  {#if !$bookmarkGroups.length}
+  {#if !$authStore}
+    <Alert color="red">
+      <span slot="icon">
+        <i class="bi bi-exclamation-circle-fill"></i>
+      </span>
+      <span class="text-lg font-medium">No user account</span>
+      <div slot="extra">
+        <div class="mt-2 mb-4 text-sm">
+          You must be signed in to use this app. Click on one of the buttons
+          above to sign in or create a new account if you're a new user.
+        </div>
+      </div>
+    </Alert>
+  {:else if !$bookmarkGroups.length}
     <BookmarkGroupFallback />
   {:else}
     {#each $bookmarkGroups as { name, id }}
--- src/lib/components/misc/Header.svelte
+++ src/lib/components/misc/Header.svelte
@@ -10,7 +10,7 @@
 
 <Navbar let:hidden let:toggle>
   {#if !$authStore && !$authStore?.emailVerified}
-    <AuthButtonGroup />
+    <AuthButtonGroup color="blue" />
   {:else}
     <Button
       color="red"
@@ -23,28 +23,30 @@
     </Button>
     <P size="lg">{$authStore.email}</P>
   {/if}
-  <div class="flex md:order-2">
-    <ModalButton text="New Bookmark Group">
-      <form
-        class="flex flex-col space-y-6"
-        action="#"
-        on:submit|preventDefault={() => createBookmarkGroup(name)}
-      >
-        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-          Create New Bookmark
-        </h3>
-        <Label class="space-y-2">
-          <span>Name</span>
-          <Input
-            type="text"
-            name="name"
-            placeholder="Example"
-            required
-            bind:value={name}
-          />
-        </Label>
-        <Button type="submit" class="w-full1">Create</Button>
-      </form>
-    </ModalButton>
-  </div>
+ {#if $authStore}
+   <div class="flex md:order-2">
+     <ModalButton text="New Bookmark Group">
+       <form
+         class="flex flex-col space-y-6"
+         action="#"
+         on:submit|preventDefault={() => createBookmarkGroup(name)}
+       >
+         <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+           Create New Bookmark
+         </h3>
+         <Label class="space-y-2">
+           <span>Name</span>
+           <Input
+             type="text"
+             name="name"
+             placeholder="Example"
+             required
+             bind:value={name}
+           />
+         </Label>
+         <Button type="submit" class="w-full1">Create</Button>
+       </form>
+     </ModalButton>
+   </div>
+ {/if}
 </Navbar>
