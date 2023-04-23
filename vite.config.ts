245ac1d4d131b43c84beb8fd2cc58d9d52fff19f
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-23 02:18:26 +0200

    changed compile target to esnext

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
+import { build, defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), crx({ manifest })],
+  build:{
+    target: 'esnext'
+  }
 });
