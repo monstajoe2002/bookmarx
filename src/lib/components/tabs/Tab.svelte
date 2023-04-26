0f22b3908e4fdc9cee91acd20e54d9603e80a4d6
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-26 12:21:25 +0200

    upgraded to minor version: 0.1.1

--- src/lib/components/tabs/Tab.svelte
+++ src/lib/components/tabs/Tab.svelte
@@ -25,10 +25,9 @@
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <h4
     class="my-auto text-xl font-bold tracking-tight text-blue-700 dark:text-white line-clamp-1 overflow-hidden whitespace-nowrap hover:underline cursor-pointer"
-    on:click={() => {
+    on:click="{() => {
       switchToTab(id);
-    }}
-  >
+    }}">
     {name}
   </h4>
 
@@ -40,16 +39,15 @@
   {#if $authStore}
     <ModalButtonWithIcon class="w-fit ml-4" color="green">
       <svelte:fragment slot="icon">
-        <i class="bi bi-bookmark-plus-fill" />
+        <i class="bi bi-bookmark-plus-fill"></i>
       </svelte:fragment>
       <svelte:fragment slot="content">
         <form
           class="flex flex-col space-y-6"
           action="#"
-          on:submit|preventDefault={() => {
+          on:submit|preventDefault="{() => {
             createBookmark(name, url, groupId);
-          }}
-        >
+          }}">
           <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
             Add Bookmark
           </h3>
@@ -60,17 +58,15 @@
               name="name"
               placeholder="Example"
               required
-              bind:value={name}
-            />
+              bind:value="{name}" />
           </Label>
           <Label>
             Select a group
             <Select
               class="mt-2"
-              items={groups}
-              bind:value={selected}
-              required
-            />
+              items="{groups}"
+              bind:value="{selected}"
+              required />
           </Label>
           <Button type="submit" color="green" class="w-full">Add</Button>
         </form>
